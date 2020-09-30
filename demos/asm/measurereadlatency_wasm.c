f02f2b97c414a9534c6524f4fa8b8531f60040ed
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2020-09-30 15:31:43 -0700

    use wasi rdtsc

--- demos/asm/measurereadlatency_wasm.c
+++ demos/asm/measurereadlatency_wasm.c
@@ -1,9 +1,12 @@
-#include <time.h>
+#include <wasi/api.h>
 #include <stdint.h>
+#include <stdio.h>
 
 uint64_t MeasureReadLatency(const void* address) {
   volatile char* addr = (volatile char*) address;
-  clock_t start = clock();
+  uint64_t start, end;
+  (void)__wasi_rdtsc(&start);
   *addr;
-  return clock() - start;
+  (void)__wasi_rdtsc(&end);
+  return end - start;
 }
