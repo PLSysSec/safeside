8d386cd5fcfed6977308a555e9c356059998f6d0
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2020-10-01 16:46:36 -0700

    build ret2spec_sa for wasm

--- build-lucet/run.sh
+++ build-lucet/run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+if [[ $1 == "pht_sa" ]]; then
+  SO=spectre_v1_pht_sa.so
+elif [[ $1 == "ret2spec_sa" ]]; then
+  SO=ret2spec_sa.so
+else
+  echo "error: Please specify an argument (either pht_sa or ret2spec_sa)"
+  exit
+fi
+
 $HOME/spectresandboxing/lucet-spectre/target/release/lucet-wasi \
     --heap-address-space "8GiB" \
     --max-heap-size "4GiB" \
     --stack-size "8MiB" \
     --dir /:/ \
-    ./build/safeside_lib_lucet.so
+    ./build/$SO
