75e9fac38b07f4266b26022e4e42c364f55ef543
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2020-10-14 20:01:40 -0700

    build with and without swivel protections

--- build-lucet/run.sh
+++ build-lucet/run.sh
@@ -5,8 +5,17 @@ if [[ $1 == "pht_sa" ]]; then
 elif [[ $1 == "ret2spec_sa" ]]; then
   SO=ret2spec_sa.so
 else
-  echo "error: Please specify an argument (either pht_sa or ret2spec_sa)"
-  exit
+  echo "error: First argument should be either pht_sa or ret2spec_sa"
+  exit 1
+fi
+
+if [[ $2 == "" ]]; then
+  echo "error: Requires a second argument, which should be the name of a directory in build/"
+  exit 1
+fi
+if [ ! -d "./build/$2" ]; then
+  echo "error: Second argument should be the name of a directory in build/"
+  exit 1
 fi
 
 $HOME/spectresandboxing/lucet-spectre/target/release/lucet-wasi \
@@ -14,4 +23,4 @@ $HOME/spectresandboxing/lucet-spectre/target/release/lucet-wasi \
     --max-heap-size "4GiB" \
     --stack-size "128MiB" \
     --dir /:/ \
-    ./build/$SO
+    ./build/$2/$SO
